35df08b5d04927fe9f62f5667c113bcae43fc830
RajeshG143 <125995440+RajeshG143@users.noreply.github.com>
2023-02-23 12:00:21 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class A4
+{
+public static void main(String args[])
+{
+Set<Integer> ts= new TreeSet<Integer>();
+    ts.add(5);
+    ts.add(6);
+    ts.add(1);
+    ts.add(53);
+    ts.add(21);
+    ts.add(4);
+System.out.println(ts);
+}
+}
